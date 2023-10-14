5023917d75bba8201dd93217c2e5c269039a99c7
skek9865 <83535254+skek9865@users.noreply.github.com>
2023-10-14 18:00:45 +0900

    Initial commit

---
+++ src/main/java/com/deving/hyoto/HyotoApplication.java
@@ -0,0 +1,13 @@
+package com.deving.hyoto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HyotoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deving/hyoto/HyotoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deving.hyoto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyotoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
